eb8f4c307cb22a6391db18c61ed65cb490dad9d7
x9316x <x9316x@gmail.com>
2023-08-04 14:11:41 +0300

    delete lesson 3.5 files

--- lesson3.5/InterfaceIterable/Controler.java
+++
@@ -1,9 +0,0 @@
-package InterfaceIterable;
-
-public class Controler {
-    private final InterfaceIterable.StudentGroupService3 studentGroupService = new InterfaceIterable.StudentGroupService3();
-
-    public void removeStudentByFIO(String lastName, String secondName, String firstName){
-        studentGroupService.removeStudentByFIO(lastName,secondName,firstName);
-    }
-}
